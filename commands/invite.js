a72c279e6d9ad8105327acc894d478002af8d9a0
Juan Pablo <34992574+JuanPablo2655@users.noreply.github.com>
2018-06-21 16:04:44 -0400

    Invite bois

---
+++ commands/invite.js
@@ -0,0 +1,8 @@
+exports.run = (client, message, args) => {
+  const Discord = require("discord.js");
+  const embed = new Discord.RichEmbed()
+
+  embed.setTitle('NationStates bot')
+  .addField('Invite the bot', 'Invite here')
+  .addField('support sever', '[Here](https://discord.gg/h6squXy)')
+}
